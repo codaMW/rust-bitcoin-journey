2d76789f5827690a9b925c48c9f18a2190bd4914
codaMW <codamw011@gmail.com>
2025-12-30 20:17:05 +0200

    added an option-result-02 exercise

---
+++ exercises/option-result-02/target/debug/deps/option_result_02.d
@@ -0,0 +1,5 @@
+C:\Users\USER\rust-bitcoin-journey\exercises\option-result-02\target\debug\deps\option_result_02.d: src\main.rs
+
+C:\Users\USER\rust-bitcoin-journey\exercises\option-result-02\target\debug\deps\option_result_02.exe: src\main.rs
+
+src\main.rs:
---
+++ exercises/option-result-02/target/debug/option-result-02.d
@@ -0,0 +1,1 @@
+C:\Users\USER\rust-bitcoin-journey\exercises\option-result-02\target\debug\option-result-02.exe: C:\Users\USER\rust-bitcoin-journey\exercises\option-result-02\src\main.rs
