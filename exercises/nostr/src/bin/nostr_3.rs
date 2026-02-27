1e3808ea187c605bb66a260beebad704492ce9f6
codaMW <codamw011@gmail.com>
2026-02-27 22:59:25 +0200

    feat: add placeholder signing method to event

---
+++ exercises/nostr/src/bin/nostr_3.rs
@@ -0,0 +1,19 @@
+impl Event {
+    fn compute_id(&self) -> String {
+        use sha2::{Sha256, Digest};
+        use serde_json::json;
+
+        let serialized = json!([
+            0,
+            self.pubkey,
+            self.created_at,
+            self.kind,
+            self.tags,
+            self.content
+        ]);
+
+        let mut hasher = Sha256::new();
+        hasher.update(serialized.to_string());
+        format!("{:x}", hasher.finalize())
+    }
+}
